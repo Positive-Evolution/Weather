45a4f6bb27f9b29e4eb2aed852bf2272ddd3683d
Taras Pypych <e32e.triptg@gmail.com>
2024-11-13 06:50:42 -0500

    Initial Commit

---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Taras Pypych on 2024-11-13.
+//
+
+import Testing
+@testable import Weather
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
